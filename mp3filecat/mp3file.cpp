b12eb5e47edd2dc6917119bad52d102a0afb4821
clfa <liangfengchen@yahoo.com>
2018-11-11 22:54:25 +0800

    实现依据mp3文件尾部ID3V1标签作者分类mp3文件功能
    实现依据mp3文件尾部ID3V1标签作者分类mp3文件功能；
    --------------------------------------------------------
    命令行执行示例：
    mp3filecat -t c|m "directory\"
    The argument \"-t c \" means copy file to category.
    The argument \"-t m \" means move file to category.
    --------------------------------------------------------

---
+++ mp3filecat/mp3file.cpp
@@ -0,0 +1,95 @@
+﻿#include "stdafx.h"
+#include "pch.h"
+#include "mp3file.h"
+
+Mp3File::Mp3File()
+{
+    memset( m_title, 0, sizeof m_title );
+    memset( m_artist, 0, sizeof m_artist );
+    memset( m_album, 0, sizeof m_album );
+    memset( m_comments, 0, sizeof m_comments );
+	m_year = 0;
+    memset( m_nullByte, 0, sizeof m_nullByte );
+	m_trackNumber = 0;
+	m_genre = 0;
+}
+
+Mp3File::~Mp3File()
+{
+}
+
+const char* Mp3File::GetTitle()const
+{
+	return m_title;
+}
+
+const char* Mp3File::GetArtist()const
+{
+	return m_artist;
+}
+
+const char* Mp3File::GetAlbum() const
+{
+	return m_album;
+}
+
+const char* Mp3File::GetComments() const
+{
+	return m_comments;
+}
+
+int Mp3File::GetYear() const
+{
+	return m_year;
+}
+
+const char Mp3File::GetGenre() const
+{
+	return m_genre;
+}
+
+int Mp3File::GetTrackNumber() const
+{
+	return m_trackNumber;
+}
+
+bool Mp3File::Open(const char* pszFullFilePath)
+{
+	if (NULL == pszFullFilePath)
+		return false;
+
+    FILE *song_file = NULL;
+    errno_t err = fopen_s( &song_file, pszFullFilePath, "r+b" );
+    if ( 0 != err ) {
+        return false;
+    }
+    fclose( song_file );
+
+	struct ID3V1 id3v1Data;
+    struct ID3V2Header id3v2HData;
+    ifstream iFile( pszFullFilePath );
+    if ( iFile ) {
+        iFile.read( reinterpret_cast<char*>(&id3v2HData), sizeof( id3v2HData ) );
+        if ( 0 == strcmp( id3v2HData.Header, "ID3" ) ) {
+            iFile.close();
+        }
+        else {
+            iFile.seekg( -128, ios::end );
+            iFile.read( reinterpret_cast<char*>(&id3v1Data), sizeof( id3v1Data ) );
+            iFile.close();
+
+            strncpy_s( m_title, sizeof( m_title ), id3v1Data.title, sizeof( m_title ) - 1 );
+            strcat_s( m_title, sizeof(m_title), "\0" );
+            strncpy_s( m_artist, sizeof(m_artist), id3v1Data.artist, sizeof( m_artist ) - 1 );
+            strcat_s( m_artist, sizeof(m_artist), "\0" );
+            strncpy_s( m_album, sizeof(m_album), id3v1Data.album, sizeof( m_album ) - 1 );
+            strcat_s( m_album, sizeof(m_album), "\0" );
+            strncpy_s( m_comments, sizeof(m_comments), id3v1Data.comments, sizeof( m_comments ) - 1 );
+            strcat_s( m_comments, sizeof(m_comments), "\0" );
+        }
+
+        return true;
+    }
+
+    return false;
+}
---
+++ mp3filecat/mp3file.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+
+using namespace std;
+
+class Mp3File
+{
+public:
+	Mp3File();
+	virtual ~Mp3File();
+
+
+	bool Open(const char* pszFullFilePath);
+
+	const char* GetTitle() const;
+	const char* GetArtist() const;
+	const char* GetAlbum() const;
+	const char* GetComments() const;
+	const char GetGenre() const;
+	int GetYear() const;
+	int GetTrackNumber() const;
+
+private:
+	char m_title[31];
+	char m_artist[31];
+	char m_album[31];
+	char m_comments[29];
+	char m_year;
+	char m_nullByte[1];
+	unsigned char m_trackNumber;
+	unsigned char m_genre;
+};
+
+struct ID3V1
+{
+    char tag[3];                // 标签头，必须为"TAG"
+	char title[30];             // 标题
+	char artist[30];            // 作者
+	char album[30];             // 专辑
+	char year[4];               // 出品年代
+	char comments[28];          // 备注
+	char nullByte;              // 保留
+	unsigned char trackNumber;  // 音轨
+	unsigned char genre;        // 类型
+};
+
+struct ID3V2Header {
+    char Header[3];             // 标签，必须为ID3
+    char Ver;                   // 版本号
+    char Reversion;             // 副版本号
+    char Flag;                  // 存放标志的字节
+    char Size[4];               // 标签大小
+};
